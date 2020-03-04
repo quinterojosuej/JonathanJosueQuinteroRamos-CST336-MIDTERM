1d8be01aea6fdcfbf1c18be91850a39f3ffc5762
EC2 Default User <ec2-user@ip-172-31-86-210.ec2.internal>
2020-03-04 23:22:05 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,38 @@
+1. 
+    a. It can be done in-line, with the style tag and with an external csss file.
+        Using an Id, Tag, or Class to give attributes to the element.
+        The precedence in order of highest to lowest is Style attribute, ID, Class and least of all element.
+    b. In the box model the difference between padding and margin is that margin 
+        refers to the distance other tags will be from the current element, and 
+        padding refers to the distance the context has to the border. They both expand
+        from the box's border
+2. 
+    a. "!==" refers to two variables/values not being the same, no matter the data
+        type. Whereas "!===" refers to the two variables/values and their data types
+        not being the same.
+    b. false
+3.
+    console.log(a1) is the complete a1 apart of indeces 0-2
+4.  
+        ----------
+        |DOCUMENT|
+        ----------
+            |
+            |
+        ------------
+        | html     |
+        ------------
+        |         |
+        |         |
+    --------    --------
+    |head  |    |body  |
+    --------    --------
+        |        |     |
+    --------  -----    ------
+    | title|  |img|    | p  |
+    --------  -----    ------
+                        |
+                       ------
+                       |span|
+                       ------
+                       
---
+++ app.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    // alert("EHELLLO");
+    $("button").on("click", function () {
+        let textIn = $("#textIn").val();
+        let bookPlace = $("#book");
+        // let infoPlace = $("#info");
+        let title = $("#title");
+        let author = $("#author");
+        let year = $("#publish_year");
+        let publisher = $("#publisher");
+        let isbn = $("#ISBN");
+        let pages = $("#pages");
+        
+        $.ajax({
+            method:     "GET",
+            url:        "https://openlibrary.org/api/books?bibkeys=ISBN:" + textIn +"&format=json&jscmd=data",
+            dataType:   "json",
+            success: function(result) {
+                
+                bookPlace.html("");
+                title.html("");
+                author.html("");
+                year.html("");
+                publisher.html("");
+                isbn.html("");
+                pages.html("");
+                
+                // console.log(result["ISBN:"+textIn]); // this is the way to be inside the object
+                
+                bookPlace.append("<img class='img-thumbnail' src=" + result["ISBN:" + textIn]["cover"]["large"] + ">");
+                
+                title.append("<div>"+result["ISBN:" + textIn]["title"] + "</div>");
+                
+                // console.log("TO GET AUTHORS");
+                // console.log(result["ISBN:" + textIn]["authors"]);
+                
+                result["ISBN:" + textIn]["authors"].forEach(function(i){
+                    author.append("<div>" + i["name"] + "</div>");
+                });
+                
+                year.append("<div>" + result["ISBN:" + textIn]["publish_date"]);
+                
+                // console.log("PUBLISHER");
+                
+                result["ISBN:" + textIn]["publishers"].forEach(function(i){
+                    publisher.append("<div>" + i["name"] + "</div>");
+                });
+                
+                isbn.append("<div>"+textIn+"</div>");
+                
+                // console.log("PAGES");
+                
+                pages.append("<div>" + result["ISBN:" + textIn]["number_of_pages"] + "</div>");
+                
+            },
+            
+            error: function(err) {
+                console.log("error is " + err);
+            }
+         
+        }); // end of ajax
+        
+    }); // end of button
+    
+});
